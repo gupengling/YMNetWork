a43e444f5ef384ae1f9c8665b08ec4a29932a18b
顾鹏凌 <pengling.gu@1mifudao.com>
2018-12-28 15:28:06 +0800

    add array

---
+++ YMPodPrivateNetWork/Classes/Extentions/NSDictionary+YMArray.h
@@ -0,0 +1,17 @@
+//
+//  NSDictionary+YMArray.h
+//  YMPodPrivateNetWork_Example
+//
+//  Created by 顾鹏凌 on 2018/12/28.
+//  Copyright © 2018 gupengling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDictionary (YMArray)
++ (NSArray *)logArray;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YMPodPrivateNetWork/Classes/Extentions/NSDictionary+YMArray.m
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+YMArray.m
+//  YMPodPrivateNetWork_Example
+//
+//  Created by 顾鹏凌 on 2018/12/28.
+//  Copyright © 2018 gupengling. All rights reserved.
+//
+
+#import "NSDictionary+YMArray.h"
+
+@implementation NSDictionary (YMArray)
++ (NSArray *)logArray {
+    return @[@"asdadsa",@"123123123"];
+}
+@end
